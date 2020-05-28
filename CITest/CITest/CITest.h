6491423aab6c0eb078115b9a04ef734ae0f5ee02
Andrei Ostafciuc <ano@miracle.dk>
2020-05-28 15:54:46 +0300

    Create sample 'CITest' project

---
+++ CITest/CITest/CITest.h
@@ -0,0 +1,19 @@
+//
+//  CITest.h
+//  CITest
+//
+//  Created by Andrei Ostafciuc on 28/05/2020.
+//  Copyright © 2020 Andrei Ostafciuc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CITest.
+FOUNDATION_EXPORT double CITestVersionNumber;
+
+//! Project version string for CITest.
+FOUNDATION_EXPORT const unsigned char CITestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CITest/PublicHeader.h>
+
+
