94bbda68c113dd07c8d14bfae52ebe512a4701d9
Andrei Ostafciuc <ano@miracle.dk>
2020-05-28 16:18:20 +0300

    Add demo 'TestFeature' with demo test file.

---
+++ CITest/CITest/Test Feature/TestFeature.swift
@@ -0,0 +1,17 @@
+//
+//  TestFeature.swift
+//  CITest
+//
+//  Created by Andrei Ostafciuc on 28/05/2020.
+//  Copyright © 2020 Andrei Ostafciuc. All rights reserved.
+//
+
+import Foundation
+
+public class TestFeature {
+    public init() { }
+
+    public func loadFeature(completion: () -> Void) {
+        completion()
+    }
+}
--- CITest/CITestTests/CITestTests.swift
+++ CITest/CITestTests/CITestTests.swift
@@ -7,28 +7,21 @@
 //
 
 import XCTest
-@testable import CITest
+import CITest
 
 class CITestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_loadFeature_executeCompletion() {
+        let sut = TestFeature()
+        let exp = expectation(description: "Wait for completion")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        var completionCalled = false
+        sut.loadFeature() {
+            completionCalled = true
+            exp.fulfill()
         }
-    }
 
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertTrue(completionCalled)
+    }
 }
